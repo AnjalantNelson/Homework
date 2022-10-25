37710e1b8255f9e16edb8a2670b94c45629ff4aa
Anjalant Nelson <nelson.anjalant4@gmail.com>
2022-10-25 14:02:30 -0600

    Methods, Revisited answered

--- answers.js
+++ answers.js
@@ -54,17 +54,37 @@
 // Yell at the Ninja Turtles
 ////////////////////////////////
 
+// const ninjaturtles = ['Donatello, Leonardo, Raphael, Michaelangelo']
+// for (let i of ninjaturtles) {
+//     i=i.toUpperCase
+// }
 
 ////////////////////////////////
 // Methods, Revisited
 ////////////////////////////////
 
-
+// const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
+// favMovies.sort()
+// favMovies.pop()
+// favMovies.push("Guardians of the Galaxy")
+// favMovies.reverse()
+// favMovies.shift()
+// favMovies.unshift()
+// for (let i = 0; i < favMovies.length; i++) {
+//     if (favMovies[i] == 'Django Unchained') {
+//         favMovies.splice();
+//         favMovies[i] = "Avatar";}
+//     }
+// // favMovies.slice(16, 0, "Avatar")
+// console.log(favMovies)
 ////////////////////////////////
 // Where is Waldo
 ////////////////////////////////
 
-
+// const whereIsWaldo = [["Timmy", "Frank"], "Eggbert",
+//                     ["Lucinda", "Jacc", "Neff", "Snoop"],
+//                     ["Petunia", ["Baked Goods", "Waldo"]]];
+// whereisWaldo.splice('Eggbert')
 ////////////////////////////////
 //  Excited Kitten
 ////////////////////////////////
