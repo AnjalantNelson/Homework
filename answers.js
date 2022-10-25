c48246803b91acd3af6f32264890f3eb46097472
Anjalant Nelson <nelson.anjalant4@gmail.com>
2022-10-25 14:32:35 -0600

    find the median answered

--- answers.js
+++ answers.js
@@ -84,12 +84,61 @@
 // const whereIsWaldo = [["Timmy", "Frank"], "Eggbert",
 //                     ["Lucinda", "Jacc", "Neff", "Snoop"],
 //                     ["Petunia", ["Baked Goods", "Waldo"]]];
-// whereisWaldo.splice('Eggbert')
+// whereIsWaldo.splice(1,1);
+//  console.log(whereIsWaldo);
+//   //neff to no one
+// whereIsWaldo[1][2] = "No One";
+// console.log(whereIsWaldo);
+//  //Where is waldo
+// console.log(whereIsWaldo[2][1][1]);
+
 ////////////////////////////////
 //  Excited Kitten
 ////////////////////////////////
 
+// let kittyTalk = ["Love me, pet me! HSSSSSS!"] 
+// for (let i = 1; i <= 20; i++ ){
+//     if (i % 2 === 0){
+//         let randomNumber = Math.floor(Math.random()*3)
+//         if(randomNumber===0){console.log("...human...why you taking pictures of me?...")} 
+//         if(randomNumber===1){console.log("...the catnip made me do it...")}
+//         if(randomNumber===2){console.log("...why does the red dot always get away...")} 
+//     } else{
+//     }
+// }
+
+//        console.log("Love me, pet me!HSSSSS"); 
 
 ////////////////////////////////
 //  Find the Median
 ////////////////////////////////
+
+// const nums = [14,11,16,15,13,16,15,17,19,11,12,14,19,11,15,17,11,18,12,17,12,71,18,15,12];
+
+// const findTheMedian = (nums) => {
+//     //Order the values from low to high
+//     const sorted = [...nums].sort((a,b) => a - b);
+//     //Is our data set odd or even-numbered?
+//     if(!(sorted.length % 2)){
+//         //If the dataset is even:
+//         //Calculate the two middle positions with the formulas (n/2) - 1 and (n/2) where n is the number of values in the dataset.
+//         let n1 = sorted[(sorted.length / 2 )- 1], n2 = sorted[sorted.length / 2];
+//         //Find the mean of the two middle values with the formula (n1 + n2) / 2
+//         return (n1 + n2) / 2;
+//     } else { 
+//         //If the dataset is odd:
+//         //Calculate the middle position with the formula n / 2 where n is the number of values in the dataset.
+//         //Return the value in the middle position
+//         return sorted[Math.floor(sorted.length / 2)];
+//     }
+// }
+
+// const unitTest1 = [350, 800, 220, 500, 130];
+// const unitTest2 = [350, 800, 220, 500, 130, 1150];
+
+// console.log(`Test One: ${findTheMedian(unitTest1)}`);
+// //expected output = 350
+// console.log(`Test Two: ${findTheMedian(unitTest2)}`);
+// //expected output = 425
+// console.log(`Answer: ${findTheMedian(nums)}`);
+// //expected output = 15;
